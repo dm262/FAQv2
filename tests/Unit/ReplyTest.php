f0f5d2da6e34c2c55d7e6b8d673857b794253b7d
dapomakanjuola <dm262@njit.edu>
2019-05-16 23:15:22 -0400

    Created Reply model

---
+++ tests/Unit/ReplyTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace Tests\Unit;
+use App\Answer;
+use App\Http\Controllers\ReplyController;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+class ReplyTest extends TestCase
+{
+    public function testReplyControllerExists(): void{
+        $this -> assertFalse(
+            class_exists("ReplyController.php")
+        );
+    }
+    public function testConstructExists(): void{
+        $this->assertFalse(
+            method_exists(ReplyController::class, "__construct")
+        );
+    }
+    public function testStoreExists(): void{
+        $this->assertFalse(
+            method_exists(ReplyController::class, "store")
+        );
+    }
+    public function testShowExists(): void{
+        $this->assertFalse(
+            method_exists(ReplyController::class, "show")
+        );
+    }
+    public function testEditExists(): void{
+        $this->assertFalse(
+            method_exists(ReplyController::class, "edit")
+        );
+    }
+    public function testUpdateExists(): void{
+        $this->assertFalse(
+            method_exists(ReplyController::class, "update")
+        );
+    }
+    public function testDestroyExists(): void{
+        $this->assertFalse(
+            method_exists(ReplyController::class, "destroy")
+        );
+    }
+}
